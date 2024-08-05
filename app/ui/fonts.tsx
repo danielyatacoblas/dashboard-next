9de74220c41f5516d78592380cb9486859112c97
danielyatab <yataco-126@hotmail.com>
2024-08-05 15:52:23 -0500

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Lusitana, Roboto } from "next/font/google";
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
